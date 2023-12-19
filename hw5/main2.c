734c153fabba7f7a281252c3231f5cfd5c3dfcc6
B1229001 <144075632+YungChiChang@users.noreply.github.com>
2023-12-19 23:32:34 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -11,11 +11,11 @@ int main()
     for (i=1; i<=300000 && fixpi!=calcpi ; i++)
     {
         PI = PI-s*4.0/(2*i+1);
-        s = s*(-1);
+        s = -s;
         calcpi = (long)(PI*100000);
     }
     
-    printf("times=%d \tvalue=%7.5f \n",i,PI);
+    printf("times=%d\t value=%.5f\n",i,PI);
     
     return 0;
 }
