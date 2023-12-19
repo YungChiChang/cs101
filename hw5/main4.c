c19271e74dbf0fc5aa89bd465b5be3fea5f7d79f
B1229001 <144075632+YungChiChang@users.noreply.github.com>
2023-12-19 21:30:24 +0800

    Create main4.c

---
+++ hw5/main4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int i = 12345;
+
+    if (i > 0) {
+        int thousands = (i / 1000) % 10;
+        int units = i % 10;
+
+        i = i - thousands * 1000 + units * 1000 + thousands - units;
+
+        printf(" %d\n", i);
+    } else {
+        printf("\n");
+    }
+
+    return 0;
+}
