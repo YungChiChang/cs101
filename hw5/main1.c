12b0462fe58b6d0267a81663dc17142ce0f0e8dd
B1229001 <144075632+YungChiChang@users.noreply.github.com>
2023-12-19 20:59:07 +0800

    Create main1.c

---
+++ hw5/main1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=7;
+    int rows;
+    int i;
+    int j;
+    int x;
+    
+    for(rows=1;rows<=n;rows++){
+        x=n+1-rows;
+        j=rows;
+        
+        for(i=1;i<=n*2;i++){
+            if(i==x&&j>0){
+                printf("%d",rows);
+                x=x+2;
+                j=j-1;
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
