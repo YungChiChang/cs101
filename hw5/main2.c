522e13f48ec84ee4d5fbc04ae378512b1854fc9d
B1229001 <144075632+YungChiChang@users.noreply.github.com>
2023-12-19 22:16:16 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main()
+{
+    double PI=4.0;
+    long fixpi=314159;
+    long calcpi=1;
+    int i;
+    float s=1.0;
+    
+    for (i=1; i<=300000 && fixpi!=calcpi ; i++)
+    {
+        PI = PI-s*4.0/(2*i+1);
+        s = s*(-1);
+        calcpi = (long)(PI*100000);
+    }
+    
+    printf("times=%d \tvalue=%7.5f \n",i,PI);
+    
+    return 0;
+}
