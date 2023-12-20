76b5bb842500e1d99fddede524094b66aea94ba2
B1229001 <144075632+YungChiChang@users.noreply.github.com>
2023-12-20 18:54:28 +0800

    Update main4.c

--- hw5/main4.c
+++ hw5/main4.c
@@ -6,11 +6,10 @@ int main() {
     if (i > 0) {
         int thousands = (i / 1000) % 10;
         int units = i % 10;
-
         i = i - thousands * 1000 + units * 1000 + thousands - units;
-
         printf(" %d\n", i);
-    } else {
+    } 
+    else {
         printf("\n");
     }
 
